1c978b1956d1bfac8c2a8ad5801fa0593e4abec4
Vlad <100291293+VladislavHonchar@users.noreply.github.com>
2024-08-10 15:30:17 +0300

    complite audio player

--- lib/models/playlist_provider.dart
+++ lib/models/playlist_provider.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:music_player/models/song_model.dart';
 
@@ -30,6 +31,129 @@ class PlaylistProvider extends ChangeNotifier{
   // current song playing index
   int? _currentSongIndex;
 
+
+  /*
+
+
+    A U D I O P L A Y E R
+  
+  
+ */
+  
+  // audio player
+  final AudioPlayer _audioPlayer = AudioPlayer();
+
+  // durations
+  Duration _currentDuration = Duration.zero;
+  Duration _totalDuration = Duration.zero;
+
+  
+  // constructor
+  PlaylistProvider(){
+    listenToDuration();
+  }
+
+  // initialliy not playing
+
+  bool _isPlaying = false;
+
+  // play the song
+
+  void play() async {
+    final String path = _playlist[_currentSongIndex!].audioPath;
+    await _audioPlayer.stop(); // stop current song
+    await _audioPlayer.play(AssetSource(path)); // play the new song
+    _isPlaying = true;
+    notifyListeners();
+  }
+
+  // pause current song
+
+  void pause() async{
+    await _audioPlayer.pause();
+    _isPlaying = false;
+    notifyListeners();
+  }
+
+  // resume playing
+
+  void resume() async{
+    await _audioPlayer.resume();
+    _isPlaying = true;
+    notifyListeners();
+  }
+
+  // pause or resume
+
+  void pauseOrResume () async{
+    if(_isPlaying){
+      pause();
+    }else{
+      resume();
+    }
+    notifyListeners();
+  }
+
+  // seek to a specific podition in the current song
+
+  void seek(Duration position) async{
+    await _audioPlayer.seek(position);
+  }
+
+  // play next song
+
+  void playNextSong() {
+    if(_currentSongIndex != null) {
+      if(_currentSongIndex! < _playlist.length -1){
+        // go to the next song if it`s not the last song
+        currentSongIndex = _currentSongIndex! + 1;
+      } else {
+        // if it`s the last song, loop back to the first song
+        currentSongIndex = 0;
+      }
+    }
+  }
+
+  // play previous song 
+
+  void playPreviousSong() async{
+    // if more than a seconds have passed, restart the current song
+    if(_currentDuration.inSeconds > 2){
+      seek(Duration.zero);
+    }
+    // if it`s within 2 second of the song, go to the previous song
+    else{
+      if(_currentSongIndex! > 0){
+        currentSongIndex = _currentSongIndex! - 1;
+      }else{
+        // if i`ts the first song loop back to last song
+        currentSongIndex = _playlist.length - 1;
+      }
+    }
+  }
+
+  // listen to durations
+
+  void listenToDuration() {
+    // listen for total duartion
+    _audioPlayer.onDurationChanged.listen((newDuration){
+      _totalDuration = newDuration;
+      notifyListeners();
+    });
+
+    // listen for current duration
+    _audioPlayer.onPositionChanged.listen((newPosition){
+      _currentDuration = newPosition;
+      notifyListeners();
+    });
+    // listen for song completion
+    _audioPlayer.onPlayerComplete.listen((event){
+      playNextSong();
+    });
+  }
+
+  //dispose audio player
+  
   /*
   
   G E T T E R S
@@ -38,7 +162,9 @@ class PlaylistProvider extends ChangeNotifier{
 
   List<Song> get playlist => _playlist;
   int? get currentSongIndex => _currentSongIndex;
-
+  bool get isPlaying => _isPlaying;
+  Duration get currentDuration => _currentDuration;
+  Duration get totalDuration => _totalDuration;
   /*
   
   S E T T E R S
@@ -48,6 +174,10 @@ class PlaylistProvider extends ChangeNotifier{
   set currentSongIndex(int? newIndex){
     // update current song index
     _currentSongIndex = newIndex;
+
+    if(newIndex != null){
+      play(); // play the song at the new index
+    }
 
     // update UI
     notifyListeners();
